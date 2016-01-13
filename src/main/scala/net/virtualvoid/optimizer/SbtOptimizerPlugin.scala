7b8ee654e69d4581e88a7fce2566a9a0ebca875f
Johannes Rudolph <johannes.rudolph@gmail.com>
2016-01-13 16:55:53 +0100

    only install Ivy settings in global settings

--- src/main/scala/net/virtualvoid/optimizer/SbtOptimizerPlugin.scala
+++ src/main/scala/net/virtualvoid/optimizer/SbtOptimizerPlugin.scala
@@ -3,8 +3,10 @@ package net.virtualvoid.optimizer
 import sbt.{ Def, AutoPlugin }
 
 object SbtOptimizerPlugin extends AutoPlugin {
-  override def projectSettings: Seq[Def.Setting[_]] =
-    IvyDownloadReporter.install() ++ Seq(
+  override def projectSettings: Seq[Def.Setting[_]] = IvyDownloadReporter.install()
+
+  override def globalSettings: Seq[Def.Setting[_]] =
+    Seq(
       IvyLockReporter.install(),
       ExecutionProgressReporter.install())
 }
